39d34a4d612e7a60accf817906ad3df8bdcc2d6a
annersp0 <petalcorin.annerachel@gmail.com>
2024-02-26 16:15:29 +0800

    second-commit

--- edit_task.php
+++ edit_task.php
@@ -44,7 +44,6 @@ if (isset($_POST["updateButton"])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
           crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
     <style>
     .insert-task-heading {
         text-align: center;
